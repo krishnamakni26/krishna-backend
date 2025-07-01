993feb268f3d18cdc1fc0e34c219a17b58cd5f8d
krishna makani <makanikrishna26@gmail.com>
2025-07-01 19:53:59 +0530

    multer middleware

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -12,7 +12,7 @@ class ApiError extends Error {
         this.success = false;
         this.error = errors
 
-        if(statck) {
+        if(stack) {
             this.stack = stack 
         } else{
             Error.captureStackTrace(this, this.constuctor)
